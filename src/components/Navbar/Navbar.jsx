a98b296f9fc793e9a4bfafe4927eea397ecefc79
rishowrav <rishowrav@gmail.com>
2024-08-25 14:22:06 +0600

    navbar section design has been completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavbarMenu } from "../../mockData/data";
+import { CiSearch } from "react-icons/ci";
+import { FaDumbbell } from "react-icons/fa";
+import { MdMenu } from "react-icons/md";
+import { PiShoppingCartThin } from "react-icons/pi";
+import ResponsiveMenu from "./ResponsiveMenu";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <nav>
+        <div className="container flex justify-between items-center py-8">
+          {/* Logo Section */}
+          <div className="text-2xl flex items-center gap-2 font-bold uppercase">
+            <FaDumbbell />
+            <p>Coders</p>
+            <p className="text-secondary">Gym</p>
+          </div>
+          {/* Menu Section */}
+          <div className="hidden md:block ">
+            <ul className="flex items-center gap-6 text-gray-600">
+              {NavbarMenu.map((menu) => (
+                <li key={menu.id}>
+                  <a
+                    href={menu.link}
+                    className="inline-block py-1 px-3 hover:text-primary font-semibold"
+                  >
+                    {menu.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* Icons Section */}
+          <div className="flex items-center gap-4">
+            <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
+              <CiSearch className="" />
+            </button>
+            <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
+              <PiShoppingCartThin className="" />
+            </button>
+            <button className="hover:bg-primary font-semibold hover:text-white rounded-md border-2 border-primary px-6 py-2 duration-200 hidden md:block">
+              Login
+            </button>
+          </div>
+          {/* Mobile hamburger menu Section */}
+          <div
+            onClick={() => setOpen(!open)}
+            className="md:hidden cursor-pointer "
+          >
+            <MdMenu className="text-4xl" />
+          </div>
+        </div>
+
+        {/* Mobile Sidebar section */}
+        <ResponsiveMenu open={open} />
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
