65a4fef04f8feeb72a19c47e439c6de3ae8e2f7f
rishowrav <rishowrav@gmail.com>
2024-08-25 16:06:24 +0600

    Hero section design has been completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import BgImage from "./assets/bg.png";
 
+const bgStyle = {
+  backgroundImage: `url(${BgImage})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundAttachment: "fixed",
+};
 export default function App() {
   return (
     <h1 className="overflow-x-hidden">
-      <Navbar />
+      <div className="" style={bgStyle}>
+        <Navbar />
+        <Hero />
+      </div>
     </h1>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { FaPlay } from "react-icons/fa";
+import HeroImg from "../../assets/dumbell.png";
+
+const Hero = () => {
+  return (
+    <>
+      <section>
+        <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[500px] relative">
+          {/* Brand Info */}
+          <div className="flex flex-col justify-center py-14 md:py-0">
+            <div className="text-center md:text-left space-y-6">
+              <h1 className="text-5xl lg:text-6xl font-bold leading-relative xl:leading-normal font-playfair">
+                Gym Gives you the perfect{" "}
+                <span className="text-primary">Health</span>
+              </h1>
+              <p className="text-gray-600 xl:max-w-[500px]">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
+                maxime sunt tempora, eaque nisi modi.
+              </p>
+              {/* button */}
+              <div className="flex justify-center gap-8 md:justify-start items-center !mt-4">
+                <button className="primary-btn flex items-center gap-2 ">
+                  Order Now
+                </button>
+                <button className=" flex items-center gap-2 justify-center">
+                  <FaPlay /> Watch Now
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* Hero Image */}
+          <div className="flex justify-center items-center">
+            <img
+              className="w-[350px] md:w-[550px] xl:w-[700px] drop-shadow"
+              src={HeroImg}
+              alt="hero image"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
