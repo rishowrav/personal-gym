5106c24cfbda73765d715b9da6a290d6d8b3f517
rishowrav <rishowrav@gmail.com>
2024-09-02 23:39:30 +0600

    Banner Section design has been completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,24 @@ import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import BgImage from "./assets/bg.png";
 import Equipments from "./components/Equipments/Equipments";
+import Banner from "./components/Banner/Banner";
+import Banner1 from "./assets/banner-1.png";
+import Banner2 from "./assets/banner-2.png";
+
+const BannerData = {
+  image: Banner1,
+  title: "The Importants To Take Care Of Yourself",
+  subtitle:
+    "The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus",
+  link: "#",
+};
+const Banner2Data = {
+  image: Banner2,
+  title: "The Importants To Take Care Of Yourself",
+  subtitle:
+    "The Importants To Take Care Of Yourself Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nam obcaecati sequi odio dolore officia magni reiciendis, architecto, eos, aut asperiores reprehenderit quas rerum omnis facilis quam eius doloribus maxime! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae vero inventore assumenda ducimus alias doloribus",
+  link: "#",
+};
 
 const bgStyle = {
   backgroundImage: `url(${BgImage})`,
@@ -18,6 +36,8 @@ export default function App() {
         <Hero />
       </div>
       <Equipments />
+      <Banner image={Banner1} {...BannerData} />
+      {/* <Banner image={Banner2} {...Banner2Data} /> */}
     </h1>
   );
 }
